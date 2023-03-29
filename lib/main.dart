7013b142cbcc248a9a22025d80d47115d54f25ee
YouHyunKim <yuhyunk7@gmail.com>
2023-03-29 14:20:28 +0900

    ScaleTransision 뼈대 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -10,33 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHome(),
+      home: Home(),
     );
   }
 }
 
-class MyHome extends StatefulWidget {
-  const MyHome({Key? key}) : super(key: key);
-
-  @override
-  State<MyHome> createState() => _MyHomeState();
-}
-
-class _MyHomeState extends State<MyHome> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        child: Container(
-          width: 200,
-          height: double.infinity,
-          color: Colors.blue,
-        ),
-      ),
-    );
-  }
-}
+st
