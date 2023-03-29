a540f2e94399ca1230dc84f362bc98a5be3f75cc
YouHyunKim <yuhyunk7@gmail.com>
2023-03-29 12:42:00 +0900

    appbar menu inout add

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHome(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
   const MyHome({Key? key}) : super(key: key);
 
   @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  Size size = Size(0, 0);
+  bool isOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    size = MediaQuery.of(context).size;
+    print("size width : ${size.width}");
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          InkWell(
+            child: Icon(Icons.menu),
+            onTap: () {
+              setState(() {
+                isOpen = !isOpen;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Text("Animation Screen"),
+          ),
+          AnimatedContainer(
+            curve: Curves.easeInOutBack, // esseInOut
+            duration: Duration(seconds: 2),
+            height: double.infinity,
+            width: size.width * (2 / 3),
+            color: Colors.blue,
+            transform: Matrix4.translationValues(isOpen ? size.width *(2/3) : size.width, 0, 0),
+          ),
+        ],
+      ),
+    );
   }
 }
