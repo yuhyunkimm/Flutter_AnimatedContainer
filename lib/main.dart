1c7d08c103b4adba309550332786bd2f875f4e43
YouHyunKim <yuhyunk7@gmail.com>
2023-03-29 14:24:26 +0900

    ScaleTransition 뼈대잡기

--- lib/main.dart
+++ lib/main.dart
@@ -15,4 +15,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-st
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
